9e668d4102912870700880412b6bce5ebdfe8abb
SilentCode1003 <j0s3ph0r3nc10@gmail.com>
2023-06-26 10:43:33 +0800

    Added Department Module

---
+++ routes/position.js
@@ -0,0 +1,141 @@
+var express = require("express");
+var router = express.Router();
+
+const mysql = require("./repository/admindb");
+const crypt = require("./repository/cryptography");
+const dictionary = require("./repository/dictionary");
+const helper = require("./repository/customhelper");
+
+/* GET home page. */
+router.get("/", function (req, res, next) {
+  res.render("position", {
+    title: req.session.title,
+    fullname: req.session.fullname,
+    roletype: req.session.roletype,
+    accesstype: req.session.accesstype,
+  });
+});
+
+function isAuthAdminUser(req, res, next) {
+  if (req.session.roletype == "Admin" || req.session.roletype == "User") {
+    next();
+  } else {
+    res.redirect("/login");
+  }
+}
+
+module.exports = router;
+
+router.get("/load", (req, res) => {
+  try {
+    let sql = `select * from master_position`;
+
+    mysql.Select(sql, "MasterPosition", (err, result) => {
+      if (err) console.error("Error: ", err);
+
+      console.log(result);
+
+      res.json({
+        msg: "success",
+        data: result,
+      });
+    });
+  } catch (error) {
+    res.json({
+      msg: error,
+    });
+  }
+});
+
+router.post("/save", (req, res) => {
+  try {
+    let positionname = req.body.positionname;
+    let status = dictionary.GetValue(dictionary.ACT());
+    let createdby = "DEV42";
+    let createddate = helper.GetCurrentDatetime();
+    let master_position = [];
+
+    master_position.push([positionname, status, createdby, createddate]);
+    mysql.InsertTable("master_position", master_position, (err, result) => {
+      if (err) console.error("Error: ", err);
+
+      console.log(result);
+      res.json({
+        msg: "success",
+      });
+    });
+  } catch (error) {
+    res.json({
+      msg: error,
+    });
+  }
+});
+
+
+router.post('/edit', (req, res) => {
+  try {
+      let positionnamemodal = req.body.positionnamemodal;
+      let positioncode = req.body.positioncode;
+      
+      let data = [positionnamemodal, positioncode];
+       
+      let sql_Update = `UPDATE master_position 
+                     SET mp_positionname = ?
+                     WHERE mp_positioncode = ?`;
+      
+      let sql_check = `SELECT * FROM master_position WHERE mp_positioncode='${positioncode}'`;
+
+      console.log(data);
+
+      mysql.Select(sql_check, 'MasterPosition', (err, result) => {
+          if (err) console.error('Error: ', err);
+
+          if (result.length != 1) {
+              return res.json({
+                  msg: 'notexist'
+              });
+          } else {
+              mysql.UpdateMultiple(sql_Update, data, (err, result) => {
+                  if (err) console.error('Error: ', err);
+
+                  console.log(result);
+
+                  res.json({
+                      msg: 'success',
+                  });
+              });
+          }
+      });
+  } catch (error) {
+      res.json({
+          msg: error
+      });
+  }
+});
+
+router.post('/status', (req, res) => {
+  try {
+      let positioncode = req.body.positioncode;
+      let status = req.body.status == dictionary.GetValue(dictionary.ACT()) ? dictionary.GetValue(dictionary.INACT()): dictionary.GetValue(dictionary.ACT());
+      let data = [status, positioncode];
+
+      let sql_Update = `UPDATE master_position 
+                     SET mp_status = ?
+                     WHERE mp_positioncode = ?`;
+
+      console.log(data);
+
+      mysql.UpdateMultiple(sql_Update, data, (err, result) => {
+          if (err) console.error('Error: ', err);
+
+          res.json({
+              msg: 'success',
+          });
+      });
+      
+  } catch (error) {
+      res.json({
+          msg: error
+      });
+  }
+});
