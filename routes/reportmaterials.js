5763b6bed05468f7813af5d9619532ed2db81f4e
SilentCode1003 <j0s3ph0r3nc10@gmail.com>
2023-10-23 09:16:01 +0800

    Added search on report materials

--- routes/reportmaterials.js
+++ routes/reportmaterials.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var router = express.Router();
-const xl = require('excel4node');
+const xl = require("excel4node");
 
 const mysql = require("./repository/cablingdb");
 const crypt = require("./repository/cryptography");
@@ -13,7 +13,6 @@ const { Validator } = require("./repository/middleware");
 router.get("/", function (req, res, next) {
   Validator(req, res, "reportmaterials");
 });
-
 
 module.exports = router;
 
@@ -98,6 +97,84 @@ router.get("/load", (req, res) => {
     res.json({
       msg: error,
     });
+  }
+});
+
+router.post("/search", (req, res) => {
+  try {
+    const { startdate, enddate } = req.body;
+    let sql = `select
+    rei_detailid as detailid,
+    rei_requestby as requestby,
+    rei_requestdate as requestdate,
+    rei_itembrand as brand,
+    rei_description as description,
+    rei_quantity as quantity,
+    rei_unit as unit,
+    rei_approvedby as approvedby,
+    rei_approveddate as approveddate,
+    cr_dr_number as drnumber,
+    cr_quantity as used,
+    cr_unit as usedunits,
+    rm_quantity as returned,
+    rm_unit as returnedunits
+    from request_equipment_item
+    left join consumption_report on rei_detailid = cr_detail_id and rei_itembrand = cr_brand and rei_description = cr_description
+    left join return_material on rei_detailid = rm_detail_id  and rei_itembrand = rm_brand and rei_description = rm_description
+    where rei_requestdate between '${startdate} 00:00' and '${enddate} 23:59'
+    order by rei_requestdate`;
+
+    console.log(sql);
+
+    mysql.SelectResult(sql, (err, result) => {
+      if (err) console.error("Error: ", err);
+
+      let data = [];
+      let reportModel = helper.ConvertToJson(result);
+      let report = reportModel.map(
+        (data) =>
+          new ReportMaterialConsumptionModel(
+            data["detailid"],
+            data["requestby"],
+            data["requestdate"],
+            data["brand"],
+            data["description"],
+            data["quantity"],
+            data["unit"],
+            data["approvedby"],
+            data["approveddate"],
+            data["drnumber"],
+            data["used"],
+            data["usedunits"],
+            data["returned"],
+            data["returnedunits"]
+          )
+      );
+
+      report.forEach((item, index) => {
+        data.push({
+          detailid: item.detailid,
+          requestby: item.requestby,
+          requestdate: item.requestdate,
+          brand: item.brand,
+          description: item.description,
+          quantity: `${item.quantity} ${item.unit}`,
+          approvedby: item.approvedby,
+          approveddate: item.approveddate,
+          drnumber: item.drnumber,
+          used: `${item.used} ${item.usedunits}`,
+          returned: `${item.returned} ${item.returnedunits}`,
+        });
+      });
+
+      console.log(data);
+      res.json({
+        msg: "success",
+        data: data,
+      });
+    });
+  } catch (error) {
+    res.json({});
   }
 });
 
